1c216be4f6e8ca4aa4a61a267e58e5c92433a3aa
shamika-anne <shamanne.sawalha@gmail.com>
2025-02-20 11:36:49 +0800

    added a view in views.py

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
+
+def index(request):
+    template = loader.get_template('recipelist.html')
+    return HttpResponse(template.render())
+
+#def tasks(request):
+    #ctx = 
