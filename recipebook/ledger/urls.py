28c84466e48b0fa9afeaf69532a4e12296ef87b0
shamika-anne <shamanne.sawalha@gmail.com>
2025-02-22 07:26:30 +0800

    added url paths for each recipe

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import recipelist
+from .views import recipelist, recipe_one, recipe_two
 
 urlpatterns = [
-    path('recipes/list/', recipelist, name='recipelist'),
+    path('recipes/list/', recipelist, name="recipelist"),
+    path('recipe/1/', recipe_one, name="recipe1"),
+    path('recipe/2', recipe_two, name="recipe2")
 ]
 
 app_name = "ledger"
