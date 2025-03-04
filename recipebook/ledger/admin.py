b10c44b8cfc2891ffb1c8c22c9b710888323f310
shamika-anne <shamanne.sawalha@gmail.com>
2025-03-04 16:25:23 +0800

    fixed list display

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -5,11 +5,17 @@ from .models import Ingredient, Recipe, RecipeIngredient
 class IngredientAdmin(admin.ModelAdmin):
     model = Ingredient
 
+    list_display = ('name', )
+
 class RecipeAdmin(admin.ModelAdmin):
     model = Recipe
 
+    list_display = ('name', )
+
 class RecipeIngredientAdmin(admin.ModelAdmin):
     model = RecipeIngredient
+
+    list_display = ('Quantity', 'Ingredient', 'Recipe', )
 
 admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(Recipe, RecipeAdmin)
