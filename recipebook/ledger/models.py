cf1b9301ae3d8032a8792d21200aff3c123a318d
shamika-anne <shamanne.sawalha@gmail.com>
2025-03-04 16:23:28 +0800

    removed unnecessary params for foreign keys

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,19 +1,33 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('ingredient_detail', arg=[str(self.name)])
+
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('recipesInDatabase')
 
 class RecipeIngredient(models.Model):
     Quantity = models.CharField(max_length=100)
     Ingredient = models.ForeignKey(
         Ingredient,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
         )
     Recipe = models.ForeignKey(
         Recipe, 
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
         )
+    
