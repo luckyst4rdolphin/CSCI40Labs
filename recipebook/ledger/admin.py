d20bd5d3e965794e9c3cdd2a338fbeec2f559b8c
shamika-anne <shamanne.sawalha@gmail.com>
2025-03-17 13:27:16 +0800

    Added docstrings for each class

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -5,25 +5,40 @@ from .models import Ingredient, Recipe, RecipeIngredient, Profile
 
 # Register your models here.
 class IngredientAdmin(admin.ModelAdmin):
+    '''
+    This creates the admin panel for the Ingredient model.
+    '''
     model = Ingredient
 
     list_display = ('name', )
 
 class RecipeAdmin(admin.ModelAdmin):
+    '''
+    This creates the admin panel for the Recipe model.
+    '''
     model = Recipe
 
     list_display = ('name', )
 
 class RecipeIngredientAdmin(admin.ModelAdmin):
+    '''
+    This creates the admin panel for the RecipeIngredient model.
+    '''
     model = RecipeIngredient
 
     list_display = ('Quantity', 'Ingredient', 'Recipe', )
 
 class ProfileInline(admin.StackedInline):
+    '''
+    This keeps the Profile model with the User model.
+    '''
     model = Profile
     can_delete = False
 
 class UserAdmin(BaseUserAdmin):
+    '''
+    This adds Profile model to the User panel.
+    '''
     inlines = [ProfileInline,]
 
 admin.site.unregister(User)
