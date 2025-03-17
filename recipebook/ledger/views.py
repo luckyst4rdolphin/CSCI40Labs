18609894be74df5c910d86bd6b4ef244cced70c5
shamika-anne <shamanne.sawalha@gmail.com>
2025-03-17 12:26:28 +0800

    Added login requirement for each view

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,12 +1,13 @@
 from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Recipe
 
-class RecipeListView(ListView):
+class RecipeListView(LoginRequiredMixin, ListView):
     context_object_name = "recipes"
     queryset = Recipe.objects.all()
     template_name = "recipes.html"
 
-class RecipeDetailView(DetailView):
+class RecipeDetailView(LoginRequiredMixin, DetailView):
     context_object_name = "recipe"
     model = Recipe
     template_name = "recipe.html"
