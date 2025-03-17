adddc1c8e7a0e0efab443d0deb5fd518593a699c
shamika-anne <shamanne.sawalha@gmail.com>
2025-03-17 13:28:17 +0800

    Added docstring for each view

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -3,11 +3,17 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Recipe
 
 class RecipeListView(LoginRequiredMixin, ListView):
+    '''
+    Creates a base view for displaying the list of recipes.
+    '''
     context_object_name = "recipes"
     queryset = Recipe.objects.all()
     template_name = "recipes.html"
 
 class RecipeDetailView(LoginRequiredMixin, DetailView):
+    '''
+    Creates a base view for displaying the contents of each recipe.
+    '''
     context_object_name = "recipe"
     model = Recipe
     template_name = "recipe.html"
