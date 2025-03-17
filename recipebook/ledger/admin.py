e3e239d18a23a5f4a377c3883b89029d5cb1583f
shamika-anne <shamanne.sawalha@gmail.com>
2025-03-17 12:18:08 +0800

    Created UserAdmin class and ProfileInline class

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Ingredient, Recipe, RecipeIngredient
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import Ingredient, Recipe, RecipeIngredient, Profile
 
 # Register your models here.
 class IngredientAdmin(admin.ModelAdmin):
@@ -17,6 +19,15 @@ class RecipeIngredientAdmin(admin.ModelAdmin):
 
     list_display = ('Quantity', 'Ingredient', 'Recipe', )
 
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+
+class UserAdmin(BaseUserAdmin):
+    inlines = [ProfileInline,]
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
