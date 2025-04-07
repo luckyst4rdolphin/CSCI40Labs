7445eab5b391b76612f8345369d0704dd24fab81
shamika-anne <shamanne.sawalha@gmail.com>
2025-04-07 08:03:23 +0800

    added new urls for each form

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import RecipeListView, RecipeDetailView
+from .views import RecipeListView, RecipeDetailView, RecipeCreateView, RecipeIngredientCreateView, IngredientCreateView, RecipeImageCreateView
 
 urlpatterns = [
     path('recipes/list/',RecipeListView.as_view(), name="recipes-list"),
-    path('recipe/<int:pk>/', RecipeDetailView.as_view(), name="recipe-detail")
+    path('recipe/<int:pk>/', RecipeDetailView.as_view(), name="recipe-detail"),
+    path('recipe/add/', RecipeCreateView.as_view(), name="recipe-create"),
+    path('recipe/add/recipeingredient/', RecipeIngredientCreateView.as_view(), name="recipeingredient-create"),
+    path('recipe/add/ingredient/', IngredientCreateView.as_view(), name="ingredient-create"),
+    path('recipe/<int:pk>/add_image/', RecipeImageCreateView.as_view(), name="recipeimage-create"),
 ]
 
 app_name = "ledger"
