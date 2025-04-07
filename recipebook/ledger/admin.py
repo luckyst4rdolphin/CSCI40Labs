5f79e161ef54dd1c6781176aa73279eec2861285
shamika-anne <shamanne.sawalha@gmail.com>
2025-04-07 08:00:42 +0800

    made recipeingredient inline with recipe model

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
-from .models import Ingredient, Recipe, RecipeIngredient, Profile
+from .models import Ingredient, Recipe, RecipeIngredient, Profile, RecipeImage
 
-# Register your models here.
 class IngredientAdmin(admin.ModelAdmin):
     '''
     This creates the admin panel for the Ingredient model.
@@ -12,6 +11,17 @@ class IngredientAdmin(admin.ModelAdmin):
 
     list_display = ('name', )
 
+class RecipeIngredientInline(admin.TabularInline):
+    '''
+    This creates the admin panel for the RecipeIngredient model.
+    '''
+    model = RecipeIngredient
+    
+    list_display = ('Quantity', 'Ingredient', 'Recipe', )
+
+class RecipeImageInline(admin.StackedInline):
+    model = RecipeImage
+    
 class RecipeAdmin(admin.ModelAdmin):
     '''
     This creates the admin panel for the Recipe model.
@@ -19,14 +29,7 @@ class RecipeAdmin(admin.ModelAdmin):
     model = Recipe
 
     list_display = ('name', )
-
-class RecipeIngredientAdmin(admin.ModelAdmin):
-    '''
-    This creates the admin panel for the RecipeIngredient model.
-    '''
-    model = RecipeIngredient
-
-    list_display = ('Quantity', 'Ingredient', 'Recipe', )
+    inlines = [RecipeImageInline, RecipeIngredientInline]
 
 class ProfileInline(admin.StackedInline):
     '''
@@ -41,8 +44,8 @@ class UserAdmin(BaseUserAdmin):
     '''
     inlines = [ProfileInline,]
 
+
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(Recipe, RecipeAdmin)
-admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
